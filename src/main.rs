fbac9dd55b02e7cbaa89c3bce24a3d5427b51857
Gabriel Galli <ggabriel96@hotmail.com>
2021-09-22 11:32:50 -0300

    feat: initial basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{Clap, AppSettings};
+
+
+#[derive(Clap)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Path of script to run
+    file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    println!("Value for path: {}", opts.file.or(Some(String::from("none"))).unwrap());
 }
