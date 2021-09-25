08b85800096e87212a576c7282ccdde06726016b
Gabriel Galli <ggabriel96@hotmail.com>
2021-09-25 01:22:30 -0300

    feat: initial implementation of expressions

---
+++ src/expr.rs
@@ -0,0 +1,47 @@
+use crate::lexing::{LiteralValue, Token};
+
+pub enum Expr {
+    Binary {
+        left: Box<Expr>,
+        op: Token,
+        right: Box<Expr>,
+    },
+    Grouping {
+        expr: Box<Expr>,
+    },
+    Literal {
+        value: Option<LiteralValue>,
+    },
+    Unary {
+        op: Token,
+        right: Box<Expr>,
+    },
+}
+
+impl std::fmt::Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        let string = match self {
+            Expr::Binary { left, op, right } => parenthesize(&op.lexeme, &[left, right]),
+            Expr::Grouping { expr } => parenthesize(&"group", &[expr]),
+            Expr::Literal { value: None } => String::from("nil"),
+            Expr::Literal {
+                value: Some(LiteralValue::Number(n)),
+            } => n.to_string(),
+            Expr::Literal {
+                value: Some(LiteralValue::String(s)),
+            } => s.clone(),
+            Expr::Unary { op, right } => parenthesize(&op.lexeme, &[right.as_ref()]),
+        };
+        write!(f, "{}", string)
+    }
+}
+
+fn parenthesize(name: &str, exprs: &[&Expr]) -> String {
+    let mut buf = vec![String::from("("), String::from(name)];
+    for expr in exprs.iter() {
+        buf.push(String::from(" "));
+        buf.push(expr.to_string());
+    }
+    buf.push(String::from(")"));
+    buf.concat()
+}
